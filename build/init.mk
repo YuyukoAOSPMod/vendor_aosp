5c54d760c6499fe29dbd5642935440bff17b5183
XayahSuSuSu <zds1249475336@gmail.com>
2022-07-30 14:08:42 +0800

    build: Add modular build hooks
    * Ref: https://github.com/ProtonAOSP/android_vendor_proton

---
+++ build/init.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2022 Acme
+# Copyright (C) 2022 YuyukoAOSPMod
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# YuyukoAOSPMod version
+-include vendor/aosp/build/tools/version.mk
---
+++ build/tools/version.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2022 Acme
+# Copyright (C) 2022 YuyukoAOSPMod
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+MOD_VERSION := 12.1
+
+ADDITIONAL_SYSTEM_PROPERTIES += \
+    ro.build.version.custom=$(MOD_VERSION)
