CSC-UI/android_vendor_csc

build: soong: Add soong bits for boardflags
Change-Id: If01db1eb6b1432a6c4d26d5c9701895ab4dc3ff5

build/soong/soong_config.mk
@@ -1,0 +1,8 @@
+_contents := $(_contents)    "Acme":{$(newline)
+
+# See build/core/soong_config.mk for the add_json_* functions you can use here.
+
+# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
+_contents := $(_contents)__SV_END
+
+_contents := $(_contents)    },$(newline)

YuyukoAOSPMod/vendor_aosp

build/soong/soong_config.mk
@@ -1,0 +1,8 @@
+_contents := $(_contents)    "Aosp":{$(newline)
+
+# See build/core/soong_config.mk for the add_json_* functions you can use here.
+
+# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
+_contents := $(_contents)__SV_END
+
+_contents := $(_contents)    },$(newline)