e8950ff7260b25d7325d8413c60e57a4e97f1c86
TH779 <i@779.moe>
2022-07-30 20:05:44 +0800

    vendor: AOSPMod: Checkout apns-conf.xml from LineageOS * From LineageOS/android_vendor_lineage@2007edd.

--- config/phone.mk
+++ config/phone.mk
@@ -8,6 +8,10 @@
 # Inherit common AOSPMod stuff
 $(call inherit-product, vendor/aosp/config/common.mk)
 
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/aosp/prebuilt/etc/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
+
 # Telephony packages
 PRODUCT_PACKAGES += \
     messaging \
