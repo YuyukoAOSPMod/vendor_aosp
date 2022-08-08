31b1b0e058aaea792e4db3cc6096980485023ab4
Yuyuko1024 <dingwenxuan4@gmail.com>
2022-08-08 10:34:56 +0800

    vendor: version.mk : Update version generate rules Set branch name to dev code name and change the Mod Version generate ruls

--- config/version.mk
+++ config/version.mk
@@ -6,7 +6,7 @@
 #
 
 # Version
-MOD_BRANCH := SnowCone-V2
+MOD_BRANCH := Sekibanki
 MOD_BUILD_TYPE := DEBUG
 MOD_DATE := $(shell date -u +%Y%m%d)
 MOD_DEVICE := $(shell echo "$(TARGET_PRODUCT)" | cut -d '_' -f 2,3)
@@ -16,7 +16,7 @@ ifeq ($(IS_RELEASE), true)
 endif
 
 MOD_VERSION := YuyukoAOSPMod-$(MOD_BRANCH)-$(MOD_DEVICE)-$(MOD_BUILD_TYPE)-$(MOD_DATE)
-MOD_DISPLAY_VERSION := YuyukoAOSPMod-$(MOD_BRANCH)-$(MOD_DATE)
+MOD_DISPLAY_VERSION := $(MOD_BRANCH)-$(MOD_DATE)
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.yuyuko.aospmod.version=$(MOD_VERSION) \
